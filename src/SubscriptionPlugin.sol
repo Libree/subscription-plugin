ef5dcb5ed1ccb4befe27709aec2c339b0b9c48f0
Ivan <ivan.alberquilla@gmail.com>
2024-03-05 18:04:35 +0100

    first commit

---
+++ src/SubscriptionPlugin.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.22;
+
+import {BasePlugin} from "@alchemy/modular-account/src/plugins/BasePlugin.sol";
+import {IPluginExecutor} from "@alchemy/modular-account/src/interfaces/IPluginExecutor.sol";
+import {
+    ManifestFunction,
+    ManifestAssociatedFunctionType,
+    ManifestAssociatedFunction,
+    PluginManifest,
+    PluginMetadata
+} from "@alchemy/modular-account/src/interfaces/IPlugin.sol";
+import {IMultiOwnerPlugin} from "@alchemy/modular-account/src/plugins/owner/IMultiOwnerPlugin.sol";
+
+/// @title SubscriptionPlugin
+/// @author Libree
+/// @notice This plugin lets us subscribe to services!
+contract SubscriptionPlugin is BasePlugin {
+    // metadata used by the pluginMetadata() method down below
+    string public constant NAME = "Subscription Plugin";
+    string public constant VERSION = "1.0.0";
+    string public constant AUTHOR = "Libree";
+
+    // this is a constant used in the manifest, to reference our only dependency: the multi owner plugin
+    // since it is the first, and only, plugin the index 0 will reference the multi owner plugin
+    // we can use this to tell the modular account that we should use the multi owner plugin to validate our user op
+    // in other words, we'll say "make sure the person calling increment is an owner of the account using our multiowner plugin"
+    uint256 internal constant _MANIFEST_DEPENDENCY_INDEX_OWNER_USER_OP_VALIDATION = 0;
+
+    // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    // ┃    Execution functions    ┃
+    // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+    // this can be called from a user op by the account owner
+    function subscribe(address service) external {}
+
+    function unsubscribe(address service) external {}
+
+    function paySubscription(address service, uint256 amount) external {}
+
+    // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    // ┃    View      functions    ┃
+    // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+    function isPaymentDue(address service) external view returns (bool) {}
+
+    function isSubscribed(address service) external view returns (bool) {}
+
+
+    // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    // ┃    Plugin interface functions    ┃
+    // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+    /// @inheritdoc BasePlugin
+    function onInstall(bytes calldata) external pure override {}
+
+    /// @inheritdoc BasePlugin
+    function onUninstall(bytes calldata) external pure override {
+        //TODO: Check if there is any subscription and cancel it
+    }
+
+    /// @inheritdoc BasePlugin
+    function pluginManifest() external pure override returns (PluginManifest memory) {
+        PluginManifest memory manifest;
+
+        manifest.dependencyInterfaceIds = new bytes4[](1);
+        manifest.dependencyInterfaceIds[0] = type(IMultiOwnerPlugin).interfaceId;
+
+        manifest.executionFunctions = new bytes4[](1);
+        manifest.executionFunctions[0] = this.subscribe.selector;
+
+        ManifestFunction memory ownerUserOpValidationFunction = ManifestFunction({
+            functionType: ManifestAssociatedFunctionType.DEPENDENCY,
+            functionId: 0, // unused since it's a dependency
+            dependencyIndex: _MANIFEST_DEPENDENCY_INDEX_OWNER_USER_OP_VALIDATION
+        });
+
+        manifest.userOpValidationFunctions = new ManifestAssociatedFunction[](1);
+        manifest.userOpValidationFunctions[0] = ManifestAssociatedFunction({
+            executionSelector: this.subscribe.selector,
+            associatedFunction: ownerUserOpValidationFunction
+        });
+
+        manifest.preRuntimeValidationHooks = new ManifestAssociatedFunction[](1);
+        manifest.preRuntimeValidationHooks[0] = ManifestAssociatedFunction({
+            executionSelector: this.subscribe.selector,
+            associatedFunction: ManifestFunction({
+                functionType: ManifestAssociatedFunctionType.PRE_HOOK_ALWAYS_DENY,
+                functionId: 0,
+                dependencyIndex: 0
+            })
+        });
+
+        manifest.permitAnyExternalAddress = true;
+        manifest.canSpendNativeToken = true;
+
+        return manifest;
+    }
+
+    /// @inheritdoc BasePlugin
+    function pluginMetadata() external pure virtual override returns (PluginMetadata memory) {
+        PluginMetadata memory metadata;
+        metadata.name = NAME;
+        metadata.version = VERSION;
+        metadata.author = AUTHOR;
+        return metadata;
+    }
+}
